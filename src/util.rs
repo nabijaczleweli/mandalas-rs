1982c1fdb644e618d98d7079b013eddf72b0c375
nabijaczleweli <nabijaczleweli@gmail.com>
2017-06-11 07:42:52 +0200

    Add CMYK->RGB and HSL->RGB conversions
    Implementing them in GenerationContext is left as an exercise to the
    reader

--- src/util.rs
+++ src/util.rs
@@ -36,3 +36,54 @@ pub fn separated_number(num: u64) -> String {
         .skip(1)
         .collect()
 }
+
+/// Translate a CMYK colour point into an RGB colour point.
+///
+/// # Examples
+///
+/// ```
+/// # use mandalas::util::cmyk_to_rgb;
+/// assert_eq!(cmyk_to_rgb([0f64, 0f64, 0f64, 0f64]), [1f64, 1f64, 1f64]);
+/// assert_eq!(cmyk_to_rgb([0f64, 1f64, 0f64, 0f64]), [1f64, 0f64, 1f64]);
+/// ```
+pub fn cmyk_to_rgb(cmyk: [f64; 4]) -> [f64; 3] {
+    [(1f64 - cmyk[0]) * (1f64 - cmyk[3]), (1f64 - cmyk[1]) * (1f64 - cmyk[3]), (1f64 - cmyk[2]) * (1f64 - cmyk[3])]
+}
+
+/// Translate an HSL colour point into an RGB colour point.
+///
+/// Based wholly on https://en.wikipedia.org/wiki/HSL_and_HSV#From_HSL.
+///
+/// # Examples
+///
+/// ```
+/// # use mandalas::util::hsl_to_rgb;
+/// assert_eq!(hsl_to_rgb([1f64, 1f64, 1f64]), [1f64, 1f64, 1f64]);
+/// assert_eq!(hsl_to_rgb([0f64, 0f64, 0f64]), [0f64, 0f64, 0f64]);
+/// ```
+pub fn hsl_to_rgb(hsl: [f64; 3]) -> [f64; 3] {
+    let h_deg = hsl[0] * 360f64;
+    let h_prim = h_deg / 60f64;
+
+    let chroma = (1f64 - (2f64 * hsl[2] - 1f64)) * hsl[1];
+    let x = chroma * (1f64 - ((h_prim % 2f64) - 1f64).abs());
+
+    let rgb1 = if 0f64 <= h_prim && h_prim <= 1f64 {
+        [chroma, x, 0f64]
+    } else if 1f64 <= h_prim && h_prim <= 2f64 {
+        [x, chroma, 0f64]
+    } else if 2f64 <= h_prim && h_prim <= 3f64 {
+        [0f64, chroma, x]
+    } else if 3f64 <= h_prim && h_prim <= 4f64 {
+        [0f64, x, chroma]
+    } else if 4f64 <= h_prim && h_prim <= 5f64 {
+        [x, 0f64, chroma]
+    } else if 5f64 <= h_prim && h_prim < 6f64 {
+        [chroma, 0f64, x]
+    } else {
+        [0f64, 0f64, 0f64]
+    };
+
+    let m = hsl[2] - (chroma / 2f64);
+    [rgb1[0] + m, rgb1[1] + m, rgb1[2] + m]
+}
